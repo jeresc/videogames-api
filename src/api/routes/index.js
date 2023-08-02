3927a01bf395b6a585ed424053ed230f7f0b89fc
jeresc <jeremiastomassrc@gmail.com>
2023-08-02 00:46:45 -0300

    feat: 🔥 remove home endpoint

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,12 +1,10 @@
 import { Router } from 'express'
-import homeRouter from './home.router.js'
 import genresRouter from './genres.router.js'
 import videogamesRouter from './videogames.router.js'
 
 const routes = (app) => {
   const router = Router()
   app.use('/api', router)
-  router.use('/', homeRouter)
   router.use('/videogames', videogamesRouter)
   router.use('/genres', genresRouter)
 }
