03323531068ec196b7cb7f988128ef426e71b4bd
jeresc <jeremiastomassrc@gmail.com>
2024-05-29 14:25:52 -0300

    refactor: ♻ fix domain on CORS

--- src/app.js
+++ src/app.js
@@ -1,38 +1,41 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import routes from "#routes"
-import passport from "#auth"
+import routes from '#routes';
+import passport from '#auth';
 import cookieParser from 'cookie-parser';
-import config from "#config";
-import { logErrors, errorHandler, ormErrorHandler } from "#middlewares";
+import config from '#config';
+import { logErrors, errorHandler, ormErrorHandler } from '#middlewares';
 
-const app = express()
+const app = express();
 
-const whitelist = config.nodeEnv === "production" ? ["https://videogames-app-jeresc.vercel.app"] : ["http://localhost:5173"]
+const whitelist =
+  config.nodeEnv === 'production'
+    ? ['https://andgames.jeresc.com']
+    : ['http://localhost:5173'];
 
 const options = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
-}
-app.use(cors(options))
+  credentials: true,
+};
+app.use(cors(options));
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cookieParser())
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
 
-routes(app)
+routes(app);
 
-app.use(passport.initialize())
+app.use(passport.initialize());
 
-app.use(logErrors)
-app.use(ormErrorHandler)
-app.use(errorHandler)
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(errorHandler);
 
-export default app
+export default app;
